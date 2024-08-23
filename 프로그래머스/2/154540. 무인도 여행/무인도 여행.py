591ab5791e1e85656a02567befd4390f469b1643
정주성 <73345198+wjdwntjd55@users.noreply.github.com>
2024-08-23 10:53:50 +0900

    [level 2] Title: 무인도 여행, Time: 19.95 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,35 +1,40 @@
 from collections import deque
 
+
 def solution(maps):
     answer = []
     graph = [list(i) for i in maps]
     visited = [[False] * len(graph[0]) for _ in range(len(graph))]
 
-    dx = [-1, 0, 1, 0] 
+    dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
 
     def bfs(x, y):
         queue = deque([(x, y)])
         visited[x][y] = True
-        total_food = int(graph[x][y])  
+        total_food = int(graph[x][y])
+
         while queue:
-            cx, cy = queue.popleft()
+            x, y = queue.popleft()
+
             for i in range(4):
-                nx, ny = cx + dx[i], cy + dy[i]
-                if 0 <= nx < len(graph) and 0 <= ny < len(graph[0]) and not visited[nx][ny]:
-                    if graph[nx][ny] != 'X': 
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if 0 <= nx < len(graph) and 0 <= ny < len(graph) and not visited[nx][ny]:
+                    if graph[nx][ny] != "X":
+                        queue.append((nx, ny))
                         visited[nx][ny] = True
                         total_food += int(graph[nx][ny])
-                        queue.append((nx, ny))
         return total_food
 
     for i in range(len(graph)):
         for j in range(len(graph[0])):
-            if graph[i][j] != 'X' and not visited[i][j]:  
+            if graph[i][j] != "X" and not visited[i][j]:
                 food = bfs(i, j)
                 answer.append(food)
 
-    if not answer:  
+    if not answer:
         return [-1]
 
-    return sorted(answer)  
+    return sorted(answer)
