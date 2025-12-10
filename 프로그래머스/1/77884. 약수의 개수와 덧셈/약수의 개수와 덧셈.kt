a5937b53e71fe7776fa23bf7fcfda0066e6c03a5
봉구형 <73345198+stellarbg@users.noreply.github.com>
2025-12-10 23:22:59 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.09 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
@@ -1,19 +1,16 @@
+import kotlin.math.sqrt
+
 class Solution {
     fun solution(left: Int, right: Int): Int {
         var answer: Int = 0
 
         for (num in left..right) {
-            var cnt = 0
-            for (i in 1..num) {
-                if (num % i == 0) {
-                    cnt += 1
-                }
-            }
+            var sqrtN = sqrt(num.toDouble())
 
-            if (cnt % 2 == 0) {
-                answer += num
-            } else {
+            if (sqrtN == sqrtN.toInt().toDouble()) {
                 answer -= num
+            } else {
+                answer += num
             }
         }
 
