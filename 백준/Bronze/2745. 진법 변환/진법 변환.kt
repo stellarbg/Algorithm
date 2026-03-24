abc87ef119ca1dcb75ccfcc6f1598ead5a9b206d
봉구형 <73345198+stellarbg@users.noreply.github.com>
2026-03-24 23:23:47 +0900

    [Bronze II] Title: 진법 변환, Time: 88 ms, Memory: 13092 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.kt
@@ -0,0 +1,10 @@
+import java.util.StringTokenizer
+
+fun main() {
+    val st = StringTokenizer(readln())
+
+    val n = st.nextToken()
+    val b = st.nextToken()
+
+    println(n.toInt(b.toInt()))
+}
